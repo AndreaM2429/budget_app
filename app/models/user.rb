5d04b3ce3854d0df28cf6a4576bd154ea98f040c
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-16 14:24:46 -0600

    Add user relations, views and controlles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :categories, foreign_key: :author_id
+  has_many :movements, foreign_key: :author_id
 end

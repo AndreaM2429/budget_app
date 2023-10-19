b365b8124e037f3a27be4c646064be6b17654aac
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-18 22:22:50 -0600

    Modify the rellationship with the correct foreign key

--- app/models/category_movement.rb
+++ app/models/category_movement.rb
@@ -1,4 +1,4 @@
 class CategoryMovement < ApplicationRecord
-  belongs_to :categories
-  belongs_to :movements
+  belongs_to :categories, class_name: 'Categories', foreign_key: :category_id
+  belongs_to :movements, class_name: 'Movement', foreign_key: :movements_id
 end

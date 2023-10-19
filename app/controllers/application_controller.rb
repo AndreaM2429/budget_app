4b08a676fdbe27a37c80c47bc290cc5f5c4943c4
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-18 22:28:07 -0600

    Add route to after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(_resource)
+    categories_path
+  end
+
 end

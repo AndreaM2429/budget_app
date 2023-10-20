e5c0d1c043b10582adf20c33b32aa8845557af87
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-19 18:53:18 -0600

    Create home request tests

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end

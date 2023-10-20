a3955cc04f066556dd46a203fbb03e1f975f88e9
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-19 22:46:36 -0600

    Quit confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable, :confirmable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :categories, foreign_key: :author_id, class_name: 'Categories'
   has_many :movements, foreign_key: :author_id, class_name: 'Movement'
--- db/schema.rb
+++ db/schema.rb
@@ -75,14 +75,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_17_204854) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
